9584d3b6f3c878e2a4d8e18ddcd47c96b6c9133d
holocaster <paulorobertosp@gmail.com>
2021-06-27 10:51:10 -0300

    Colocando properties para kafka

--- src/main/java/br/gov/inst/atlan/configserver/ConfigServerApplication.java
+++ src/main/java/br/gov/inst/atlan/configserver/ConfigServerApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		Arrays.stream(args).sorted(Comparator.reverseOrder());
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
